6e5a6f2e8e656cda0e87c1a0ba255c22ce3f61cc
vcgdev <vcg_cruz@hotmail.com>
2022-03-10 21:44:36 -0600

    practice with state and event handlers

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import NewExpense from "./components/new-expense/NewExpense";
 const App = () => {
   const expenses = [
     { title: "Paper", amount: 294.67, date: new Date(2022, 2, 28), id: 1 },
-    { title: "Car insurance", amount: 14.67, date: new Date(2022, 4, 28), id: 2 },
+    {
+      title: "Car insurance",
+      amount: 14.67,
+      date: new Date(2022, 4, 28),
+      id: 2,
+    },
     { title: "Pencil", amount: 100, date: new Date(2022, 1, 27), id: 3 },
     { title: "Computer", amount: 649.12, date: new Date(2022, 3, 29), id: 4 },
   ];
@@ -12,10 +17,10 @@ const App = () => {
 
   return (
     <div>
-      <NewExpense onAddExpense={addExpenseHandler}/>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses expenses={expenses} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -1,19 +1,30 @@
 import ExpenseItem from "./ExpenseItem";
 import Card from "../ui/Card";
-import './Expenses.css'
+import "./Expenses.css";
+import ExpensesFilter from "../expenses-filter/ExpensesFilter";
+import { useState } from "react";
 
 const Expenses = (props) => {
-    return (
-        <Card className="expenses">
-            {
-                props.expenses.map((expense) => (
-                    <ExpenseItem key={expense.id} title={expense.title}
-                    amount={expense.amount}
-                    date={expense.date} />
-                ))
-            }
-        </Card>
-    )
-}
+    const [filteredYear, setFilterYear] = useState('2021');
+    const yearFilterChangeHandler = (yearToFilter) => {
+        console.log(`Should search by year: ${yearToFilter}`);
+        setFilterYear(yearToFilter);
+    };
+  return (
+    <div>     
+      <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onYearFilterChange={yearFilterChangeHandler}></ExpensesFilter>
+        {props.expenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
+  );
+};
 
 export default Expenses;
