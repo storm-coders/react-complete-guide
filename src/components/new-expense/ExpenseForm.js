6f6c615eeb2478b05b281e377aaf27c911ac075f
vcgdev <vcg_cruz@hotmail.com>
2022-03-30 20:39:35 -0600

    enforce number to ammount

--- src/components/new-expense/ExpenseForm.js
+++ src/components/new-expense/ExpenseForm.js
@@ -35,7 +35,7 @@ const ExpenseForm = (props) => {
     event.preventDefault();
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
     props.onSaveExpenseData(expenseData);
